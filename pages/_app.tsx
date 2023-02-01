35fde3fab5453648f4677c09db25343d3dbc10ed
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2023-02-01 09:20:33 -0400

    Fixing the viewport for mobile devices

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,14 @@ import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    // We execute the same script as before
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
   }, []);
   return (
     <AppProvider>
