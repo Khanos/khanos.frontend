fc086a584dbb6d36256d63f35f4bfa210d022c7d
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2023-02-01 09:08:04 -0400

    Quick fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AppProvider } from "@/context/appContext";
-
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
   return (
     <AppProvider>
       <Component {...pageProps} />
