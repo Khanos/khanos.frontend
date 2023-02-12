b7010d6441cd924f23d08b0f0302a30940bb33f7
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2023-02-12 17:58:59 -0400

    adding google analytics

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,18 @@ export default function Header() {
             <meta name="description" content="A khanos fun personal project" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
+            {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-525CEBLYVE"></script>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-525CEBLYVE');
+                    `,  
+                }}
+            />
         </Head>
     )
 };
