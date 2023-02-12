9709c1429e51f917e9c75cbc25b2e016eaf94b4d
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2023-02-12 17:40:08 -0400

    Adding a download function for the generated images, bumping the resolution of the images

--- components/DalleGalleryItem.tsx
+++ components/DalleGalleryItem.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import styles from '@/styles/Dalle.module.css'
 import type { dalleImageType } from '@/context/types'
-import { useEffect } from 'react'
+import { useState } from 'react'
 
 interface Props {
   image: dalleImageType['image']
@@ -10,17 +10,36 @@ interface Props {
 }
 
 export default function DalleGalleryItem({ text, image, index }: Props) {
+  const [loading, setIsLoading] = useState(false);
+  function downloadImage(base64String: string) {
+    setIsLoading(true);
+    setTimeout(() => {
+      const link = document.createElement('a');
+      link.download = 'image.png';
+      link.href = base64String;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      setIsLoading(false);
+    } , 1000);
+  }
+
   return (
     <div className={`${styles['gallery-item']} ${index === 0 ? styles['gallery-item-current'] : ''}`}>
       <div className={styles['gallery-item-text']}>
         {text}
+        {loading ?
+          <span className={styles['gallery-item-download-loading']}>⏳</span>
+          :
+          <span className={styles['gallery-item-download']} onClick={() => downloadImage(image)}>⤵️</span>
+        } 
       </div>
       <Image
         className={styles['gallery-item-image']}
         src={image}
         alt={text}
-        width={index === 0 ? 450 : 300}
-        height={index === 0 ? 450 : 300}
+        width={1024}
+        height={1024}
       />
     </div>
   )
