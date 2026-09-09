2eda23d3443ea8ed4c1a50cf9bbe50f978e9e584
Epilef Alejandro Rodriguez Benavides <epilef.rodriguez@gmail.com>
2026-09-09 11:38:08 +0200

    feat: upgrade Astro and Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import robotsTxt from "astro-robots-txt";
 import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
