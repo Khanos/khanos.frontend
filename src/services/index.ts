d6cbcf4c75bd8cac2981c5ad59d9ba137bf0eb87
Epilef Alejandro Rodriguez Benavides <epilef.rodriguez@gmail.com>
2026-09-09 11:22:45 +0200

    fix: convert fetch functions to async in src/services/index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,67 +3,47 @@ import type { githubCommitType, urlShortenerType, urlListType } from '../types';
 const githubEndPoint = "https://khanos-backend.herokuapp.com/api/github/getCommits/";
 const urlShortenerEndPoint = "https://khanos-backend.herokuapp.com/api/url/";
 
-export const searchGithubCommits = (search: string) => {
-  return fetch(`${githubEndPoint}${search}`)
-    .then((response) => response.json() as Promise<{ items: githubCommitType[]; total_count: number; }>)
-    .then((data) => {
-      return data;
-    }).catch((error) => {
-      console.log(error);
-    });
+export const searchGithubCommits = async (search: string) => {
+  const response = await fetch(`${githubEndPoint}${search}`)
+  const data = await response.json() as Promise<{ items: githubCommitType[]; total_count: number; }>
+  return data;
 }
 
-export const getUrlData = ( urlId: string ) => {
+export const getUrlData = async ( urlId: string ) => {
   const apiUrl = new URL(urlShortenerEndPoint + urlId);
-  return fetch(apiUrl)
-    .then((response) => response.json() as Promise<urlShortenerType>)
-    .then((data) => {
-      return data;
-    }).catch((error) => {
-      console.log(error);
-    });
+  const response = await fetch(apiUrl);
+  const data = await response.json() as Promise<urlShortenerType>;
+  return data;
 }
 
-export const gerUrlList = () => {
+export const gerUrlList = async () => {
   const apiUrl = new URL(urlShortenerEndPoint);
-  return fetch(apiUrl)
-    .then((response) => response.json() as Promise<urlListType>)
-    .then((data) => {
-      return data;
-    }).catch((error) => {
-      console.log(error);
-    });
-};
+  const response = await fetch(apiUrl);
+  const data = await response.json() as Promise<urlListType>;
+  return data;
+}
 
-export const addShortUrl = (url: string) => {
+export const addShortUrl = async (url: string) => {
   const apiUrl = new URL(urlShortenerEndPoint + 'create');
-  return fetch(apiUrl, {
+  const response = await fetch(apiUrl, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ original_url: url })
-  })
-    .then((response) => response.json() as Promise<urlShortenerType>)
-    .then((data) => {
-      return data;
-    }).catch((error) => {
-      console.log(error);
-    });
-};
+  });
+  const data = await response.json() as Promise<urlShortenerType>;
+  return data;
+}
 
-export const deleteShortUrl = (urlId: string) => {
+export const deleteShortUrl = async (urlId: string) => {
   const apiUrl = new URL(urlShortenerEndPoint + `delete/${urlId}`);
-  return fetch(apiUrl, {
+  const response = await fetch(apiUrl, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json'
     }
-  })
-    .then((response) => response.json() as Promise<urlShortenerType>)
-    .then((data) => {
-      return data;
-    }).catch((error) => {
-      console.log(error);
-    });
-};
+  });
+  const data = await response.json() as Promise<urlShortenerType>;
+  return data;
+}
